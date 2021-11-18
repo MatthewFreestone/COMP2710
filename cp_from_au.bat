dedc22456f06cf738a394b22fb6dae5d8a60d115
Matthew Freestone <matthewfreestone1593@gmail.com>
2021-11-18 11:46:24 -0600

    Finished project 5

---
+++ cp_from_au.bat
@@ -0,0 +1,1 @@
+scp -r maf0083@scp.eng.auburn.edu:/home/u2/maf0083/Project5 ~/Desktop/Homework/COMP2710/Project5/deliverables
--- cp_to_au.bat
+++ cp_to_au.bat
@@ -1,1 +1,1 @@
-scp -r ~/Desktop/Homework/COMP2710/Project4 maf0083@scp.eng.auburn.edu:/home/u2/maf0083
+scp -r ~/Desktop/Homework/COMP2710/Project5 maf0083@scp.eng.auburn.edu:/home/u2/maf0083
